cbf7f661ce313894a03169e8b64e2a26ece4efb4
William Beard <disenno@gmail.com>
2013-12-24 11:57:07 -0600

    Cleanup doc in app.py

--- app.py
+++ app.py
@@ -27,43 +27,30 @@ def handle_invalid_usage(error):
 def getroot():
 	return "The root of all evil"
 
-# GET /collection/raw
+# GET /collection
 # Optional querystring values
 # sort, direction, offset, limit
-# Returns [
-#	{
-#		folderId: <id>
-#	},
-#	{
-#		folderId: <id>		
-#}]
 @app.route("/<collection>", methods=['GET'])
 def get_collection(collection):
 	return Route_Handlers.get_collection(collection)
 
 # GET /collection/id
-# aliases /collection/raw/id
 @app.route("/<collection>/<id>", methods=['GET'])
 def get_doc_by_id(collection, id):
 	return Route_Handlers.get_doc_by_id(collection, id)
 
 # POST /collection
-# Expects JSON in body
-# Returns full object + new id
 @app.route("/<collection>", methods=['POST'])
 def post_doc(collection):
 	return Route_Handlers.post_doc(collection)
 
 # PUT /collection
-# Expects JSON in body
 # Upserts
-# Returns full object
 @app.route("/<collection>/<id>", methods=['PUT'])
 def put_doc(collection, id):
 	return Route_Handlers.put_doc(collection, id)
 
 # DELETE /collection/id
-# Returns 204
 @app.route("/<collection>/<id>", methods=['DELETE'])
 def delete_doc(collection, id):
 	return Route_Handlers.delete_doc(collection, id)
